c30b9eb4856eedecdcb2fce7e819ba12f887a9f8
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2024-05-05 21:15:17 -0500

    Se añadió botón para ruta de administrador (testing)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header"
 import Property from "./Components/Property"
 import listProperties from '../Schema/ListProperties.json'
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 function App() {
   const [header, setheader] = useState(false);
 
@@ -25,6 +26,7 @@ function App() {
   return (
     <>
     <Header header={header}/>
+    <Link to={'/admin/users'}><div className=" bottom-8 px-3 py-2 font-bold rounded-3xl saturate-200 backdrop-blur-xl shadow-2xl right-10 fixed bg-rose-400 text-white">Admin</div></Link>
     <div className={`${header ? "mt-[273px]": ""} container mx-auto`}>
       <div className='grid grid-cols-1 xl:grid-cols-5  md:grid-cols-3 sm:grid-cols-2  gap-x-4 gap-y-8 mt-8'>
         {listProperties.map((property) => {
